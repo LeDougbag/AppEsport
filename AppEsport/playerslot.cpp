018534d3de8696a5b0ef13f5338587ab16310d73
Fappinator3000 <alessio.francinelli@epsi.fr>
2018-03-09 12:32:27 +0100

    msg

--- AppEsport/playerslot.cpp
+++ AppEsport/playerslot.cpp
@@ -1,6 +1,12 @@
 #include "playerslot.h"
 
-PlayerSlot::PlayerSlot(QWidget *parent = Q_NULLPTR) : QWidget(parent)
+PlayerSlot::PlayerSlot(QWidget *parent = Q_NULLPTR, int _id, QString _name, QString _team) : QWidget(parent)
 {
+    this->setFixedSize(1200, 70);
 
+    /*QPixmap pix("../images/background.png");
+
+    background = new QLabel(this);
+    background->setFixedSize(pix.width(), pix.height());
+    background->setPixmap(pix);*/
 }
--- AppEsport/playerslot.h
+++ AppEsport/playerslot.h
@@ -4,20 +4,24 @@
 #include <QWidget>
 #include <QString>
 #include <QLabel>
+#include "advancedbutton.h"
 
 class PlayerSlot : public QWidget
 {
 public:
-    PlayerSlot(QWidget *parent = Q_NULLPTR);
+    PlayerSlot(QWidget *parent = Q_NULLPTR, int _id, QString _name, QString _team);
 
 private:
-
-    int     id;
-    QString name;
-    QString team;
-    QLabel  *background;
+    int             id;
+    QString         name;
+    QString         team;
+    QLabel          *background;
+    QLabel          *textString;
+    QLabel          *mana;
+    AdvancedButton  *erase;
 
 private slots:
+
 
 };
 
